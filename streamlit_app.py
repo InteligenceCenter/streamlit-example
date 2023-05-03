2fb04bdbd24f41c4470d913590091291d84f1023
InteligenceCenter <132439507+InteligenceCenter@users.noreply.github.com>
2023-05-03 11:48:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,24 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+    
+
+from streamlit_observable import observable
+
+a = st.slider("Alex", value=30)
+b = st.slider("Brian", value=20)
+c = st.slider("Craig", value=50)
+
+observable("Example Updatable Bar Chart", 
+    notebook="@juba/updatable-bar-chart", 
+    targets=["chart", "draw"], 
+    redefine={
+        "data": [
+            {"name": "Alex", "value": a},
+            {"name": "Brian", "value": b},
+            {"name": "Craig", "value": c}
+        ],
+    },
+    hide=["draw"]
+)
