214b5f185262f6c8067ec8efcd4d31a6d91032de
InteligenceCenter <132439507+InteligenceCenter@users.noreply.github.com>
2023-05-03 12:26:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,19 +38,21 @@ with st.echo(code_location='below'):
         .encode(x='x:Q', y='y:Q'))
 
 
-import observable
 
-@st.cache
-def get_trader_joes():
-    # a lot of code...
-    return df
+from streamlit_observable import observable
 
-df = get_trader_joes()
-
-observable("Trader Joes Voronoi Map", 
-    notebook="@mbostock/u-s-voronoi-map-o-matic", 
-    targets=["map"],
-    redefine={
-        "data": df[["longitude", "latitude", "name"]].to_dict(orient="records")
-    }
+observers = observable("Example form", 
+    notebook="@mbostock/form-input",
+    targets=["viewof object"],
+    observe=["object"]
 )
+
+o = observers.get("object")
+
+if o is not None:
+    st.write("message: **'{message}'**, hue: '{hue}', size: '{size}', emojis: '{emojis}'".format(
+        message=o.get("message"),
+        hue=o.get("hue"),
+        size=o.get("size"),
+        emojis=str(o.get("emojis"))
+    ))
