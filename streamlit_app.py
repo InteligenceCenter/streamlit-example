0aea7771c0cabf76310cd480829291a5dea615fc
InteligenceCenter <132439507+InteligenceCenter@users.noreply.github.com>
2023-05-03 12:07:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,15 +43,14 @@ a = st.slider("Alex", value=30)
 b = st.slider("Brian", value=20)
 c = st.slider("Craig", value=50)
 
-observable("Example Updatable Bar Chart", 
-    notebook="@juba/updatable-bar-chart", 
-    targets=["chart", "draw"], 
-    redefine={
-        "data": [
-            {"name": "Alex", "value": a},
-            {"name": "Brian", "value": b},
-            {"name": "Craig", "value": c}
-        ],
-    },
-    hide=["draw"]
-)
+observable("Example Updatable Bar Chart",
+           notebook="@juba/updatable-bar-chart",
+           targets=["chart", "draw"],
+           redefine={"data": [
+               {"name": "Alex", "value": a},
+               {"name": "Brian", "value": b},
+               {"name": "Craig", "value": c}
+           ],
+                    },
+           hide=["draw"]
+          )
