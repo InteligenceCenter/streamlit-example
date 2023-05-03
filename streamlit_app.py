ed39446433d877b94ab30013dc4d2f29e72bb0a8
InteligenceCenter <132439507+InteligenceCenter@users.noreply.github.com>
2023-05-03 12:19:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,11 +39,12 @@ with st.echo(code_location='below'):
  """
 
 from streamlit_observable import observable
-a = st.slider("Alex", value=30)
-b = st.slider("Brian", value=20)
-c = st.slider("Craig", value=50)
+with st.echo(code_location='below'):
+    a = st.slider("Alex", value=30)
+    b = st.slider("Brian", value=20)
+    c = st.slider("Craig", value=50)
 
-observable("Example Updatable Bar Chart",
+    observable("Example Updatable Bar Chart",
            notebook="@juba/updatable-bar-chart",
            targets=["chart", "draw"],
            redefine={"data": [
