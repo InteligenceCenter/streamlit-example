d1cd7a7df22c75d37491616f4a2a4ea8e7f72869
InteligenceCenter <132439507+InteligenceCenter@users.noreply.github.com>
2023-05-03 12:18:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-
+"""
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
@@ -36,7 +36,7 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
- 
+ """
 
 from streamlit_observable import observable
 
@@ -51,15 +51,11 @@ a = st.slider("Alex", value=30)
 b = st.slider("Brian", value=20)
 c = st.slider("Craig", value=50)
 
-observable("Example Updatable Bar Chart", 
-    notebook="@juba/updatable-bar-chart", 
-    targets=["chart", "draw"], 
-    redefine={
-        "data": [
-            {"name": "Alex", "value": 10},
-            {"name": "Brian", "value": 20},
-            {"name": "Craig", "value": 40}
-        ],
-    },
-    hide=["draw"]
-)
+observable("Example Updatable Bar Chart",
+           notebook="@juba/updatable-bar-chart",
+           targets=["chart", "draw"],
+           redefine={"data": [
+               {"name": "Alex", "value": a},
+               {"name": "Brian", "value": b},
+               {"name": "Craig", "value": c}
+           ],},hide=["draw"])
